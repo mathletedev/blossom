e94d981a9ff2c140f0918c6a28b54c5a67e9a0fb
Neal Wang <nealwang.sh@protonmail.com>
2024-05-03 23:18:27 -0700

    player sprite

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,6 +2,8 @@ extends CharacterBody2D
 
 @export var speed: float = 100
 
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+
 var input_direction := Vector2.ZERO
 
 
@@ -9,5 +11,10 @@ func _process(_delta):
 	input_direction = Input.get_vector("left", "right", "up", "down")
 
 	velocity = input_direction.normalized() * speed
+
+	if velocity.x > 0:
+		sprite.flip_h = false
+	elif velocity.x < 0:
+		sprite.flip_h = true
 
 	move_and_slide()
