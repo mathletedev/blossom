7ba7c8a8ac674b8e05690b05bf153093e4a30de8
Neal Wang <nealwang.sh@protonmail.com>
2024-05-04 18:47:48 -0700

    animations

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
 @export var speed: float = 100
+@export var idle_deadzone: float = 0.1
 
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 
@@ -11,6 +12,11 @@ func _process(_delta):
 	input_direction = Input.get_vector("left", "right", "up", "down")
 
 	velocity = input_direction.normalized() * speed
+
+	if velocity.length() > idle_deadzone:
+		sprite.play("run")
+	else:
+		sprite.play("idle")
 
 	if velocity.x > 0:
 		sprite.flip_h = false
